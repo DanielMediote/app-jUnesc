f7ef57bca1ef404071e4f67a5ca970cf253183f9
DanielMeidiote <d4n1b0yofficial@gmail.com>
2019-05-07 21:52:30 -0400

    comentario

---
+++ jUnesc/database/migrations/2019_03_27_233404_create_presentations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePresentationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('presentations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->string('authors', 150);
+            $table->string('resume', 200);
+            $table->string('local', 100);
+            $table->integer('vacant');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('presentations');
+    }
+}
